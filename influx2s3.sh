87c8cb97e9602fb2b06071f278944a297e3c7653
followCrom <followcrom@gmail.com>
2025-08-28 05:50:39 +0100

    Update .gitignore to include logs directory and modify influx2s3.sh for 30-day backup range

--- influx2s3.sh
+++ influx2s3.sh
@@ -5,31 +5,35 @@ set -o pipefail
 # Make sure `~/.influxdbv2/configs` is properly configured with your active profile
 
 S3_BUCKET="s3://followcrom/cromwell"
-FILENAME="cromwell_data-$(date +%Y-%m-%d).json"
-FILENAME="${FILENAME%.json}.gz"  # Compress the JSON file
+# FILENAME="cromwell_data-$(date +%Y-%m-%d).json"
+# FILENAME="${FILENAME%.json}.gz"  # Compress the JSON file
 # FILENAME="cromwell_data-$(date +%Y-%m-%d).csv"
+
+FILENAME="cromwell_data-30-days-until-$(date +%Y-%m-%d).csv"
+
+
 S3_PATH="$S3_BUCKET/$FILENAME"
 
 # Time range for the backup. Examples:
 # -30d = Last 30 days
 # -7d  = Last 7 days
 # -1d  = Yesterday
-TIME_RANGE="-1d"
+TIME_RANGE="-30d"
 
 echo "📦 Starting InfluxDB backup to S3 for time range: $TIME_RANGE"
 
 # Execute Influx query and upload csv to S3
-# influx query "from(bucket: \"cromwell-fitbit-2\") |> range(start: $TIME_RANGE)" \
-#   | aws s3 cp - "$S3_PATH" --content-type text/csv
+influx query "from(bucket: \"cromwell-fitbit-2\") |> range(start: $TIME_RANGE)" \
+  | aws s3 cp - "$S3_PATH" --content-type text/csv
   
 # Execute Influx query and upload json to S3
 # influx query "from(bucket: \"cromwell-fitbit\") |> range(start: $TIME_RANGE)" \
 #   | aws s3 cp - "$S3_PATH" --content-type application/json
 
 # Compress the output and upload to S3
-influx query "from(bucket: \"cromwell-fitbit-2\") |> range(start: $TIME_RANGE)" \
-  | gzip \
-  | aws s3 cp - "${S3_PATH}.gz" --content-encoding gzip --content-type application/json
+# influx query "from(bucket: \"cromwell-fitbit-2\") |> range(start: $TIME_RANGE)" \
+#   | gzip \
+#   | aws s3 cp - "${S3_PATH}.gz" --content-encoding gzip --content-type application/json
 
 # Check exit status
 if [ $? -eq 0 ]; then
